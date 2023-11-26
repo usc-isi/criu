df5e80d497f6705aac84b51864b0d0c4e41d98e7
Yixue Zhao <felicitia2010@gmail.com>
2023-11-25 20:39:44 -0500

    Create build_libbsd.sh
    add scripts to cross compile libbsd (and libmd that libbsd requires) for RISC-V
    
    Signed-off-by: Yixue Zhao <felicitia2010@gmail.com>

---
+++ scripts/hpsc-CC/build_libbsd.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "running script: build_libbsd.sh"
+
+. ./config.sh
+. ./util.sh
+
+LIBBSD_GIT_URL="https://gitlab.freedesktop.org/libbsd/libbsd.git"
+
+LIBMD_GIT_URL="https://git.hadrons.org/git/libmd.git" # libbsd requires libmd
+
+build_libmd_riscv64 () {
+    cd $BUILD_ROOT_DIR
+    git clone $LIBMD_GIT_URL
+    cd libmd
+
+    ./autogen
+    mkdir -p riscv64_build
+    cd riscv64_build
+    
+    CC=riscv64-unknown-linux-gnu-gcc \
+    CXX=riscv64-unknown-linux-gnu-g++ \
+    ../configure --prefix=$BUILD_ROOT_DIR/riscv64_pb_install \
+    --enable-static --host=riscv64-unknown-linux-gnu
+
+    make && make install
+}
+
+build_libbsd_riscv64 () {
+    cd $BUILD_ROOT_DIR
+    git clone $LIBBSD_GIT_URL
+    cd libbsd
+
+    ./autogen
+    mkdir -p riscv64_build
+    cd riscv64_build
+    
+    CC=riscv64-unknown-linux-gnu-gcc \
+    CXX=riscv64-unknown-linux-gnu-g++ \
+    LDFLAGS=-L$BUILD_ROOT_DIR/riscv64_pb_install/lib \
+    CPPFLAGS=-I$BUILD_ROOT_DIR/riscv64_pb_install/include \
+    ../configure --prefix=$BUILD_ROOT_DIR/riscv64_pb_install \
+    --enable-static --host=riscv64-unknown-linux-gnu
+
+    make && make install
+}
+
+main () {
+
+    
+    case $TARGET_ARCH in
+        
+        "riscv64")
+            printf "${BCyan}building libmd for $TARGET_ARCH${Color_Off}\n"
+            measure_func_time build_libmd_riscv64
+            printf "${BCyan}building libbsd for $TARGET_ARCH${Color_Off}\n"
+            measure_func_time build_libbsd_riscv64  
+            ;;
+
+        *)
+            echo "the target architecture $TARGET_ARCH is not supported, exit the program..."
+            exit
+            ;;
+    esac
+}
+
+main
